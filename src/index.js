e13aa081776e9176bcf5695069b3fe3ab451e3da
ameen-alam <ameenalam202@gmail.com>
2022-07-27 17:41:22 +0500

    add webpack and babel

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, onAuthStateChanged } from "firebase/auth"
-const firebaseApp = initializeApp({
 
+import logo from './assets/logo.svg'
+const logImg = document.getElementById("logo");
+logImg.src = logo
+
+const firebaseApp = initializeApp({
+    apiKey: "AIzaSyBIgXhGsj-8HtHKQAsbCWu2di_cLjiz6V4",
+    authDomain: "adminpannel-2625e.firebaseapp.com",
+    databaseURL: "https://adminpannel-2625e.firebaseio.com",
+    projectId: "adminpannel-2625e",
+    storageBucket: "adminpannel-2625e.appspot.com",
+    messagingSenderId: "388760984590",
+    appId: "1:388760984590:web:cbf7be52964d07e9a6b05f"
 })
 const auth = getAuth(firebaseApp)
 onAuthStateChanged(auth, user => {
     if(user != null){
         console.log("logged in!");
     } else {
-        console.log("no user");
+        console.log("no user!!");
     }
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,54 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const WebpackAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 module.exports = {
     mode: 'development',
     devtool: 'eval-source-map',
-    entry: './src/index.js',
+    entry: {
+        bundle: path.resolve(__dirname, './src/index.js',) 
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    }
+        filename: '[name][contenthash].js',
+        clean: true,
+        assetModuleFilename: '[name][ext]',
+    },
+    devtool: 'source-map',
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist')
+        },
+        port: 3000,
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true, 
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    }
+                }
+            },
+            {
+                test: /\.(png|svg|jpeg|jpg|gif)$/i,
+                type: 'asset/resource'
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Firebase App",
+            filename: "index.html",
+            template: 'src/index.html'
+        }),
+        new WebpackAnalyzerPlugin(),
+    ],
 };
